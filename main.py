42ff5651cdef2aeda8c012924db39554ab7762ce
Patataman <lezdshas@gmail.com>
2016-06-11 21:36:19 +0200

    cosas

--- main.py
+++ main.py
@@ -174,7 +174,8 @@ def getByName(name):
 			#	return json.JSONEncoder().encode({"result": [parser]})
 			parser = []
 			for i in students:
-				persona = "%s, %s, %s" % (i.name, i.uid, i.email)
+				persona = [i.name, i.uid, i.email]
+				#persona = json.JSONEncoder().encode({"name": i.name, "nia": i.uid, "email": i.email})
 				#parser.append(json.dumps([i.name, i.uid, i.email], separators=(',',':')))
 				parser.append(persona)
 			return json.JSONEncoder().encode({"result": parser})
